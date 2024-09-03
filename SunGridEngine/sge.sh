ba7215b4c2c1f05c529a6b2de90b1b92f3532bee
AmauryMaros <maros.amaury@gmail.com>
2024-09-03 13:48:33 -0400

    Update sge.sh

--- SunGridEngine/sge.sh
+++ SunGridEngine/sge.sh
@@ -4,15 +4,17 @@
 # Set parameters
 #######################################
 
-#$ -cwd -V              # Use the current working directory and inherit current environment variables
-#$ -l mem_free=16G      # Set memory requirement
-#$ -P project_name      # Specify project name
-#$ -q all.q             # Select queue
-#$ -N job_name          # Name of the job
-#$ -j y                 # Join stdout and stderr
-#$ -o path/to/logs      # set the log file location for stdout
-#$ -e path/to/logs      # set the log file location for stderr
-#$ -t 1-10              # Define an array job with tasks 1 to 10
+#$ -cwd -V                        # Use the current working directory and inherit current environment variables
+#$ -b y                           # Submit a binary program
+#$ -l mem_free=16G                # Set memory requirement
+#$ -P project_name                # Specify project name
+#$ -q all.q                       # Select queue
+#$ -q threaded.q -pe thread 32    # multithreaded job 
+#$ -N job_name                    # Name of the job
+#$ -j y                           # Join stdout and stderr
+#$ -o path/to/logs                # set the log file location for stdout
+#$ -e path/to/logs                # set the log file location for stderr
+#$ -t 1-10                        # Define an array job with tasks 1 to 10
 
 #######################################
 # Conda activation
@@ -31,4 +33,4 @@ cd /path/to/working/directory
 infile=`sed -n "$SGE_TASK_ID p" list_of_samples.txt`
 
 # Run the script
-Rscript path/to/Rscript.R
+Rscript path/to/Rscript.R path/to/samples/${infile}.fasta
