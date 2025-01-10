f83ac4a52b10fe234884b417a40967c60dba132f
Amaury Maros <maros.amaury@gmail.com>
2025-01-10 10:28:54 -0500

    Update parse_jsons.sh

--- antismash/02_pipeline/parse_jsons.sh
+++ antismash/02_pipeline/parse_jsons.sh
@@ -7,8 +7,14 @@
 
 cd /local/scratch/amaros/antismash/02_pipeline/
 
-#/usr/local/packages/python-3.11/bin/python3 01_process_antismash_output.py /local/scratch/amaros/antismash/results_antismash/ /local/scratch/amaros/antismash/03_results/jsons/
+# Filter JSON with/without results
+/usr/local/packages/python-3.11/bin/python3 01_process_antismash_output.py /local/scratch/amaros/antismash/results_antismash/ /local/scratch/amaros/antismash/03_results/jsons/
 
-/usr/local/packages/python-3.11/bin/python3 02_process_antismash_json.py /local/scratch/amaros/antismash/03_results/jsons/ /local/scratch/amaros/antismash/03_results/csv_files/
+# Parse JSON
+# /usr/local/packages/python-3.11/bin/python3 02_process_antismash_json.py /local/scratch/amaros/antismash/03_results/jsons/ /local/scratch/amaros/antismash/03_results/csv_files/
 
+# Python scripts
+# /usr/local/packages/python-3.11/bin/python3 03_get_ctg_coordinates.py
+# /usr/local/packages/python-3.11/bin/python3 04_get_ctg_sequences.py
+# /usr/local/packages/python-3.11/bin/python3 05_blast_ctg.py
 
